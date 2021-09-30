2b2ee52220c22512569b3465874dafae34f7224f
Tran Huy Duan <tranhuy.duan@fusang.co>
2021-09-30 23:46:42 +0700

    Update bitcoin

--- src/blockchain/bitcoin/bitcoin.controller.ts
+++ src/blockchain/bitcoin/bitcoin.controller.ts
@@ -10,27 +10,27 @@ export class BitcoinController {
     private readonly bitcoinService: BitcoinService,
   ) { }
   @Get('/:testnet/address/:mnemonic/:index')
-  async getAddress(@Param('mnemonic') mnemonic: string, @Param('index') index: number, @Param('testnet') testnet: boolean): Promise<string> {
-    return await this.bitcoinService.getAddress(mnemonic, index, testnet);
+  async getAddress(@Param('mnemonic') mnemonic: string, @Param('index') index: number, @Param('testnet') testnet: string): Promise<string> {
+    return await this.bitcoinService.getAddress(mnemonic, index, testnet === 'true');
   }
 
   @Get('/:testnet/:address/balance')
-  async getBalance(@Param('address') address: string, @Param('testnet') testnet: boolean): Promise<string> {
-    return await this.bitcoinService.getBalance(address, testnet);
+  async getBalance(@Param('address') address: string, @Param('testnet') testnet: string): Promise<string> {
+    return await this.bitcoinService.getBalance(address, testnet === 'true');
   }
 
   @Get('/:testnet/:address/transactions')
-  async getTransactionsByAddress(@Param('address') address: string, @Param('testnet') testnet: boolean): Promise<any> {
-    return await this.bitcoinService.getTransactionsByAddress(address, testnet);
+  async getTransactionsByAddress(@Param('address') address: string, @Param('testnet') testnet: string): Promise<any> {
+    return await this.bitcoinService.getTransactionsByAddress(address, testnet === 'true');
   }
 
   @Post('/:testnet/broadcast/:signedTx')
-  async boradcast(@Param('signedTx') signedTx: string, @Param('testnet') testnet: boolean): Promise<string> {
-    return await this.bitcoinService.broadcast(signedTx, testnet);
+  async boradcast(@Param('signedTx') signedTx: string, @Param('testnet') testnet: string): Promise<string> {
+    return await this.bitcoinService.broadcast(signedTx, testnet === 'true');
   }
 
   @Post('/:testnet/transaction')
-  async send(@Body() transaction: TxDto, @Param('testnet') testnet: boolean): Promise<string> {
-    return await this.bitcoinService.sendCoin(transaction, testnet);
+  async send(@Body() transaction: TxDto, @Param('testnet') testnet: string): Promise<string> {
+    return await this.bitcoinService.sendCoin(transaction, testnet === 'true');
   }
 }
